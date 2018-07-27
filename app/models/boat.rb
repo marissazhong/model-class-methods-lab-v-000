e4fb0ecd7cc40224f65fbd6445fb1efaf2aba5ef
Marissa Zhong <mwz2101@columbia.edu>
2018-07-27 18:08:23 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -28,6 +28,6 @@ class Boat < ActiveRecord::Base
   end
 
   def self.with_three_classifications
-    joins(:classifications).where(classifications: count(3))
+    joins(:classifications).where("classifications.count = 3")
   end
 end
