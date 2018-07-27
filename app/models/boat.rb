41ebf2f83be13c0f8bf781cf1de0a9935261e2a1
Marissa Zhong <mwz2101@columbia.edu>
2018-07-27 17:43:37 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,21 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+
+  end
+
+  def self.dinghy
+
+  end
+
+  def ship
+
+  end
+
+  def last_three_alphabetically
+
+  end
+  
 end
